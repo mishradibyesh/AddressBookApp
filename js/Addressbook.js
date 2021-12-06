0c16324d3fb19424aaeb9c0c254221d217177897
mishradibyesh <dibyeshmishra207@gmail.com>
2021-12-06 05:34:46 +0530

    ADDED : code to populate AddressBook

--- js/Addressbook.js
+++ js/Addressbook.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     const nameError = document.querySelector('.name-error');
     name.addEventListener('input', function() {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}[ : ][A-Z]{1}[a-z]{2,}$');
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
         if (nameRegex.test(name.value)) nameError.textContent = "";
         else nameError.textContent = "Name is Incorrect";
     });
@@ -22,4 +22,62 @@ window.addEventListener('DOMContentLoaded', (event) => {
         if (addressRegex.test(address.value)) addressError.textContent = "";
         else addressError.textContent = "Address is Incorrect";
     });
+
 });
+
+// save functionality of submit button defined 
+const save = () => {
+    try {
+        let addressbookData = createAddressbook();
+        createAndUpdateStorage(addressbookData);
+    } catch (e) {
+        return;
+    }
+}
+
+const createAddressbook = () => {
+    let addressbookData = new AddressbookData();
+    try {
+        addressbookData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    addressbookData.name = getInputValueById('#name');
+    addressbookData.address = getInputValueById('#address');
+    addressbookData.phone = getInputValueById('#phone');
+    addressbookData.city = getInputValueById('#City');
+    addressbookData._state = getInputValueById('#State');
+    addressbookData.zip = getInputValueById('#ZipCode');
+
+    alert(addressbookData.toString());
+    return addressbookData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+// saving the data into local storage
+function createAndUpdateStorage(addressbookData) {
+    let addressbookDataList = JSON.parse(localStorage.getItem("AddressbookDataList"));
+    if (addressbookDataList != undefined) {
+        addressbookDataList.push(addressbookData);
+    } else {
+        addressbookDataList = [addressbookData];
+    }
+    alert(addressbookDataList.toString());
+    localStorage.setItem("AddressbookDataList", JSON.stringify(addressbookDataList));
+}
